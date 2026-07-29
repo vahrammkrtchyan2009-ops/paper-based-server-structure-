865e2306c6c4d9fcb4a14630044c496d50f43cb8
vahrammkrtchyan2009-ops <vahrammkrtchyan2009@gmail.com>
2026-07-30 01:31:16 +0400

    Create script.sh
    script for startup

---
+++ script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# server startup script with tuned Aikar's flags for Java 21
+java -Xms2G -Xmx2G -XX:+UseG1GC -XX:+ParallelRefProcEnabled \
+-XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions \
+-XX:+DisableExplicitGC -XX:+AlwaysPreTouch \
+-jar purpur-1.21.jar nogui
